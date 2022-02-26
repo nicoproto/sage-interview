7cde20d7d034e046eb141cd6e478da7eff1def4b
Nico Proto <nicolasproto@gmail.com>
2022-02-26 19:10:58 -0300

    FEAT: Rails Internationalization and added spanish translations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  resources :queries, only: :create do
-    post '/send', to: 'queries#send_facts'
+  scope '(:locale)', locale: /en|es/ do
+    root to: 'pages#home'
+    resources :queries, only: :create do
+      post '/send', to: 'queries#send_facts'
+    end
   end
 end
