06ee41593a6ccae95d1c516f9b107a7a44ca7f3b
Nico Proto <nicolasproto@gmail.com>
2022-02-26 16:42:04 -0300

    TEST: Added model tests for Query

--- spec/models/query_spec.rb
+++ spec/models/query_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Query, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:kind) }
+    it { should validate_length_of(:input).is_at_least(3)}
+  end
 end
