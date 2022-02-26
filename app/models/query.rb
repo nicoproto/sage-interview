57c8d0addf4ad83b518766e130d82de0eea9de2b
Nico Proto <nicolasproto@gmail.com>
2022-02-26 17:23:03 -0300

    FEAT: Added validations for Query model regarding kind and input attributes

--- app/models/query.rb
+++ app/models/query.rb
@@ -2,4 +2,7 @@ class Query < ApplicationRecord
   serialize :facts, Array
 
   enum kind: [:random, :category, :words]
+
+  validates_presence_of :kind
+  validates_length_of :input, minimum: 3
 end
--- spec/controllers/queries_controller_spec.rb
+++ spec/controllers/queries_controller_spec.rb
@@ -1,13 +1,10 @@
 require "rails_helper"
-require "pry"
 
 describe QueriesController do
   describe "#create" do
     context "with valid query" do
       it "responds with facts" do
         post :create, params: { type: "words", query: "cow", format: 'json' }
-
-        binding.pry
 
         resp = JSON.parse(response.body)
         expect(resp.facts.lenght).to be > 0
