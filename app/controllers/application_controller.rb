99a8e22ee71257ea69fcb97953753a7b81724db7
Nico Proto <nicolasproto@gmail.com>
2022-02-26 19:16:52 -0300

    FEAT: Added change language button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   around_action :switch_locale
 
   def switch_locale(&action)
-    locale = params[:locale] || I18n.default_locale
-    I18n.with_locale(locale, &action)
+    @locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(@locale, &action)
   end
 
   def default_url_options
