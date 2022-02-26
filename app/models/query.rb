b7dd4801aaa7b3ea61de76a06410a7a96b386d86
Nico Proto <nicolasproto@gmail.com>
2022-02-26 18:36:51 -0300

    FEAT: Added button to get a random fact

--- app/models/query.rb
+++ app/models/query.rb
@@ -4,5 +4,4 @@ class Query < ApplicationRecord
   enum kind: [:random, :category, :words]
 
   validates_presence_of :kind
-  validates_length_of :input, minimum: 3
 end
--- spec/models/query_spec.rb
+++ spec/models/query_spec.rb
@@ -3,6 +3,5 @@ require 'rails_helper'
 RSpec.describe Query, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:kind) }
-    it { should validate_length_of(:input).is_at_least(3)}
   end
 end
